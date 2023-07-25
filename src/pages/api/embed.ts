0f1c1e2e137b1c9ca338b9cbd558ef4462a4506e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2023-07-25 17:26:23 +0900

    comment out unused API

--- src/pages/api/embed.ts
+++ src/pages/api/embed.ts
@@ -1,9 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { embed } from "../../utils/embed";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { embed } from "../../utils/embed";
 
-const handle = async (req: NextApiRequest, res: NextApiResponse) => {
-  const textToEmbed = req.body.text;
-  const openAIEmbedding = await embed(textToEmbed);
-  res.status(200).json(openAIEmbedding);
-};
-export default handle;
+// const handle = async (req: NextApiRequest, res: NextApiResponse) => {
+//   const textToEmbed = req.body.text;
+//   const openAIEmbedding = await embed(textToEmbed);
+//   res.status(200).json(openAIEmbedding);
+// };
+// export default handle;
